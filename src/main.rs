8c0eddee79183434320409039bf6921289fad1e6
Jesse Luehrs <doy@tozt.net>
2018-11-04 19:50:48 -0500

    initial implementation

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate linkify;
+extern crate open;
+
+use std::io::prelude::*;
+
+fn read_stdin() -> String {
+    let mut input = String::new();
+    let stdin = std::io::stdin();
+    let mut stdin_handle = stdin.lock();
+    stdin_handle.read_to_string(&mut input).unwrap();
+    return input;
+}
+
+fn main() {
+    let input = read_stdin();
+    let finder = linkify::LinkFinder::new();
+    let links: Vec<_> = finder.links(&input).map(|l| l.as_str()).collect();
+    if links.len() > 0 {
+        open::that(links[links.len() - 1]).unwrap();
+    }
+}
